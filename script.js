9b0a4b9a20b50848401d4aef095dbb0699b8ac15
Carlos Caicedo <calicheccmusica@gmail.com>
2021-12-22 01:46:02 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("You are in the Matrix!!!")
+
+function disappear(element) {
+    element.remove();
+}
+
+// function search(){
+//     var element = document.querySelector("#searchbar");
+//     alert(element.value);
+// }
+
+// function vote(id){
+//     var element = document.querySelector(id);
+//     var newText = parseInt(element.innerText) + 1;
+//     console.log(newText);
+//     element.innerText = newText;
+// }
